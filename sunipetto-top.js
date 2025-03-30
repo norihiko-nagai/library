0857544b04886e7e7d42aa7e7e203559fecf8572
Nagai <nagai.dor@gmail.com>
2025-03-30 13:59:35 +0900

    トップページ用の構造化データ

---
+++ sunipetto-top.js
@@ -0,0 +1,15 @@
+<script type="application/ld+json">
+{
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "name": "サイトの名前",
+    "url": "https://www.example.com/",
+    "description": "サイト全体の説明",
+    "image": "https://www.example.com/logo.jpg",
+    "potentialAction": {
+        "@type": "SearchAction",
+        "target": "https://www.example.com/search?q={search_term_string}",
+        "query-input": "required name=search_term_string"
+    }
+}
+</script>
