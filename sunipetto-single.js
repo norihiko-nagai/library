6ba5940d8d49da27946f0a1f6f32c9fe2903f697
Nagai <nagai.dor@gmail.com>
2025-03-30 13:59:55 +0900

    個別ページ用の構造化データ

---
+++ sunipetto-single.js
@@ -0,0 +1,33 @@
+<script type="application/ld+json">
+{
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    "name": "ページのタイトル",
+    "url": "https://www.example.com/page-url",
+    "description": "このページの説明",
+    "image": "https://www.example.com/page-thumbnail.jpg",
+    "breadcrumb": {
+        "@type": "BreadcrumbList",
+        "itemListElement": [
+            {
+                "@type": "ListItem",
+                "position": 1,
+                "name": "ホーム",
+                "item": "https://www.example.com/"
+            },
+            {
+                "@type": "ListItem",
+                "position": 2,
+                "name": "カテゴリ",
+                "item": "https://www.example.com/category"
+            },
+            {
+                "@type": "ListItem",
+                "position": 3,
+                "name": "ページのタイトル",
+                "item": "https://www.example.com/page-url"
+            }
+        ]
+    }
+}
+</script>
